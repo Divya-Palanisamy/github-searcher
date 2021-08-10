66af36d4704f62074f1ca0e42effec84366cb9d1
Divya <divyapalanisamy8@gmail.com>
2021-08-10 10:58:47 +0530

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: 'AIzaSyBHGrUhpStd0mxdbYJVqwXvsPOuhaPUpj4',
+    authDomain: 'github-angular-8a60e.firebaseapp.com',
+    projectId: 'github-angular-8a60e',
+    storageBucket: 'github-angular-8a60e.appspot.com',
+    messagingSenderId: '215178344230',
+    appId: '1:215178344230:web:39d8ce61134cbf36ff33e7'
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,14 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
- firebase:{
-    apiKey: "AIzaSyBHGrUhpStd0mxdbYJVqwXvsPOuhaPUpj4",
-    authDomain: "github-angular-8a60e.firebaseapp.com",
-    projectId: "github-angular-8a60e",
-    storageBucket: "github-angular-8a60e.appspot.com",
-    messagingSenderId: "215178344230",
-    appId: "1:215178344230:web:39d8ce61134cbf36ff33e7"
+  production: false,
+  firebase:{
+    apiKey: 'AIzaSyBHGrUhpStd0mxdbYJVqwXvsPOuhaPUpj4',
+    authDomain: 'github-angular-8a60e.firebaseapp.com',
+    projectId: 'github-angular-8a60e',
+    storageBucket: 'github-angular-8a60e.appspot.com',
+    messagingSenderId: '215178344230',
+    appId: '1:215178344230:web:39d8ce61134cbf36ff33e7'
   },
 };
 
